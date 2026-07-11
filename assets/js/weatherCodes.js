b9415d734ad3424cf292fb88b484a1329a01f517
hitbit72 <hitbit2@hotmail.com>
2026-07-11 07:51:02 +0200

    update Night ICONS

--- assets/js/weatherCodes.js
+++ assets/js/weatherCodes.js
@@ -8,229 +8,143 @@ const WEATHER_CODES = {
     codes: {
         0: {
             text: "Despejado",
-            icon: "clear_day.svg"
+            icon: "clear_day.svg",
+            nicon: "clear_night.svg"
             },
         1: {
             text: "Mayormente despejado",
-            icon: "partly_cloudy.svg"
+            icon: "mostly_clear_day.svg",
+            nicon: "mostly_clear_night.svg"
         },
         2: {
             text: "Parcialmente nublado",
-            icon: "partly_cloudy.svg"
+            icon: "partly_cloudy_day.svg",
+            nicon: "partly_cloudy_night.svg"
         },
         3: {
             text: "Cubierto",
-            icon: "cloudy.svg"
+            icon: "cloudy.svg",
+            nicon: "cloudy.svg"
         },
         45: {
             text: "Niebla",
-            icon: "fog.svg"
+            icon: "fog.svg",
+            nicon: "fog.svg"
         },
         48: {
             text: "Niebla con escarcha",
-            icon: "fog.svg"
+            icon: "fog.svg",
+            nicon: "fog.svg"
         },
         51: {
             text: "Llovizna ligera",
-            icon: "drizzle.svg"
+            icon: "drizzle.svg",
+            nicon: "drizzle.svg"
         },
         53: {
             text: "Llovizna",
-            icon: "drizzle.svg"
+            icon: "drizzle.svg",
+            nicon: "drizzle.svg"
         },
         55: {
             text: "Llovizna intensa",
-            icon: "drizzle.svg"
+            icon: "drizzle.svg",
+            nicon: "drizzle.svg"
         },
         56: {
             text: "Llovizna helada",
-            icon: "flurries.svg"
+            icon: "flurries.svg",
+            nicon: "flurries.svg"
         },
         57: {
             text: "Llovizna helada fuerte",
-            icon: "flurries.svg"
+            icon: "flurries.svg",
+            nicon: "flurries.svg"
         },
         61: {
             text: "Lluvia ligera",
-            icon: "drizzle.svg"
+            icon: "drizzle.svg",
+            nicon: "drizzle.svg"
         },
         63: {
             text: "Lluvia",
-            icon: "rain.svg"
+            icon: "rain.svg",
+            nicon: "rain_night.svg"
         },
         65: {
             text: "Lluvia intensa",
-            icon: "rain.svg"
+            icon: "heavy_rain.svg",
+            nicon: "rain_night.svg"
         },
         66: {
             text: "Lluvia helada",
-            icon: "rain_with_snow.svg"
+            icon: "rain_with_snow.svg",
+            nicon: "rain_with_snow.svg"
         },
         67: {
             text: "Lluvia helada intensa",
-            icon: "rain_with_snow.svg"
+            icon: "rain_with_snow.svg",
+            nicon: "rain_with_snow.svg"
         },
         71: {
             text: "Nevada ligera",
-            icon: "flurries.svg"
+            icon: "flurries.svg",
+            nicon: "flurries.svg"
         },
         73: {
             text: "Nevada",
-            icon: "snow.svg"
+            icon: "snow.svg",
+            nicon: "snow.svg"
         },
         75: {
             text: "Nevada intensa",
-            icon: "snow.svg"
+            icon: "heavy_snow.svg",
+            nicon: "heavy_snow.svg"
         },
         77: {
             text: "Granizo",
-            icon: "hail.svg"
+            icon: "icy.svg",
+            nicon: "icy.svg"
         },
         80: {
             text: "Chubascos ligeros",
-            icon: "showers.svg"
+            icon: "drizzle.svg",
+            nicon: "drizzle.svg"
         },
         81: {
             text: "Chubascos",
-            icon: "showers.svg"
+            icon: "isolated_thunderstorms.svg",
+            nicon: "isolated_thunderstorms_night.svg"
         },
         82: {
             text: "Chubascos intensos",
-            icon: "showers.svg"
+            icon: "isolated_thunderstorms.svg",
+            nicon: "isolated_thunderstorms_night.svg"
         },
         85: {
             text: "Nevadas",
-            icon: "snow.svg"
+            icon: "snow.svg",
+            nicon: "snow_night.svg"
         },
         86: {
             text: "Nevadas intensas",
-            icon: "snow.svg"
+            icon: "heavy_snow.svg",
+            nicon: "heavy_snow.svg"
         },
         95: {
             text: "Tormenta",
-            icon: "storm.svg"
+            icon: "thunderstorms.svg",
+            nicon: "thunderstorms_night.svg"
         },
         96: {
             text: "Tormenta con granizo",
-            icon: "storm.svg"
+            icon: "thunderstorms.svg",
+            nicon: "thunderstorms_night.svg"
         },
         99: {
             text: "Tormenta severa",
-            icon: "storm.svg"
-        }
-    },
-    codes_night: {
-        0: {
-            text: "Despejado",
-            icon: "clear_night.svg"
-            },
-        1: {
-            text: "Mayormente despejado",
-            icon: "partly_cloudy_night.svg"
-        },
-        2: {
-            text: "Parcialmente nublado",
-            icon: "partly_cloudy_night.svg"
-        },
-        3: {
-            text: "Cubierto",
-            icon: "cloudy.svg"
-        },
-        45: {
-            text: "Niebla",
-            icon: "fog.svg"
-        },
-        48: {
-            text: "Niebla con escarcha",
-            icon: "fog.svg"
-        },
-        51: {
-            text: "Llovizna ligera",
-            icon: "drizzle.svg"
-        },
-        53: {
-            text: "Llovizna",
-            icon: "drizzle.svg"
-        },
-        55: {
-            text: "Llovizna intensa",
-            icon: "drizzle.svg"
-        },
-        56: {
-            text: "Llovizna helada",
-            icon: "flurries.svg"
-        },
-        57: {
-            text: "Llovizna helada fuerte",
-            icon: "flurries.svg"
-        },
-        61: {
-            text: "Lluvia ligera",
-            icon: "drizzle.svg"
-        },
-        63: {
-            text: "Lluvia",
-            icon: "rain.svg"
-        },
-        65: {
-            text: "Lluvia intensa",
-            icon: "rain.svg"
-        },
-        66: {
-            text: "Lluvia helada",
-            icon: "rain_with_snow.svg"
-        },
-        67: {
-            text: "Lluvia helada intensa",
-            icon: "rain.svg"
-        },
-        71: {
-            text: "Nevada ligera",
-            icon: "flurries.svg"
-        },
-        73: {
-            text: "Nevada",
-            icon: "snow.svg"
-        },
-        75: {
-            text: "Nevada intensa",
-            icon: "snow.svg"
-        },
-        77: {
-            text: "Granizo",
-            icon: "hail.svg"
-        },
-        80: {
-            text: "Chubascos ligeros",
-            icon: "showers.svg"
-        },
-        81: {
-            text: "Chubascos",
-            icon: "showers.svg"
-        },
-        82: {
-            text: "Chubascos intensos",
-            icon: "showers.svg"
-        },
-        85: {
-            text: "Nevadas",
-            icon: "snow.svg"
-        },
-        86: {
-            text: "Nevadas intensas",
-            icon: "snow.svg"
-        },
-        95: {
-            text: "Tormenta",
-            icon: "storm.svg"
-        },
-        96: {
-            text: "Tormenta con granizo",
-            icon: "storm.svg"
-        },
-        99: {
-            text: "Tormenta severa",
-            icon: "storm.svg"
+            icon: "thunderstorms.svg",
+            nicon: "thunderstorms_night.svg"
         }
     },
     get(code) {
